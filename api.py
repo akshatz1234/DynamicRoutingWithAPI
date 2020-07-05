a2ad5f657f1bd7f395336e251c72e0f94c28cc02
Akshat Zala <akshat.z@arnekt.com>
2020-07-05 12:31:42 +0530

    Added remaining files

---
+++ api.py
@@ -0,0 +1,83 @@
+# from flask import Flask
+# from flask_restful import reqparse, abort, Api, Resource
+
+# app = Flask(__name__)
+# api = Api(app)
+
+# TODOS = {
+#     'todo1': {'task': 'build an API'},
+#     'todo2': {'task': '?????'},
+#     'todo3': {'task': 'profit!'},
+# }
+
+
+# def abort_if_todo_doesnt_exist(todo_id):
+#     if todo_id not in TODOS:
+#         abort(404, message="Todo {} doesn't exist".format(todo_id))
+
+# parser = reqparse.RequestParser()
+# parser.add_argument('task')
+
+
+# # Todo
+# # shows a single todo item and lets you delete a todo item
+# class Todo(Resource):
+#     def get(self, todo_id):
+#         abort_if_todo_doesnt_exist(todo_id)
+#         return TODOS[todo_id]
+
+#     def delete(self, todo_id):
+#         abort_if_todo_doesnt_exist(todo_id)
+#         del TODOS[todo_id]
+#         return '', 204
+
+#     def put(self, todo_id):
+#         args = parser.parse_args()
+#         task = {'task': args['task']}
+#         TODOS[todo_id] = task
+#         return task, 201
+
+
+# # TodoList
+# # shows a list of all todos, and lets you POST to add new tasks
+# class TodoList(Resource):
+#     def get(self):
+#         return TODOS
+
+#     def post(self):
+#         args = parser.parse_args()
+#         todo_id = int(max(TODOS.keys()).lstrip('todo')) + 1
+#         todo_id = 'todo%i' % todo_id
+#         TODOS[todo_id] = {'task': args['task']}
+#         return TODOS[todo_id], 201
+
+# class TodoNext(Resource):
+#     def __init__(self, **kwargs):
+#         # smart_engine is a black box dependency
+#         self.smart_engine = kwargs['smart_engine']
+
+#     def get(self):
+#         return self.smart_engine.next_todo()
+
+# ##
+# ## Actually setup the Api resource routing here
+# ##
+# api.add_resource(TodoList, '/todos')
+# api.add_resource(Todo, '/todos/<todo_id>')
+
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
+from flask_restful import Resource, fields, marshal_with
+
+resource_fields = {
+    'name': fields.String,
+    'address': fields.String,
+    'date_updated': fields.DateTime(dt_format='rfc822'),
+}
+
+class Todo(Resource):
+    @marshal_with(resource_fields, envelope='resource')
+    def get(self, **kwargs):
+        return db_get_todo() 
